762b2436d33a5808949e2d3daa674898199f8d8a
briglobeinc <briglobeinc@gmail.com>
2018-09-24 16:29:27 +0300

    added front end structure

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import Argon from "./plugins/argon-kit";
 
 Vue.config.productionTip = false;
-
+Vue.use(Argon);
 new Vue({
   router,
-  store,
-  render: (h) => h(App)
+  render: h => h(App)
 }).$mount("#app");
