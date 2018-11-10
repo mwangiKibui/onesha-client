faac6d86d98e570070a3213570c993d82f521914
Danny Sofftie <dankim761@gmail.com>
2018-11-10 12:40:36 +0300

    Exclude development files from master

--- server/README.md
+++
@@ -1,5 +0,0 @@
-# Onesha Backend Server
-
-<!-- Read me after project begins -->
-
-> Read me after project begins
--- server/configs/config.ts
+++
@@ -1,8 +0,0 @@
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
-export function config(): string {
-    return `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSOWRD}@${process.env.MYSQL_HOST}:3306/${process.env.MYSQL_DATABASE}?connectionLimit=10&dateStrings=true`;
-}
