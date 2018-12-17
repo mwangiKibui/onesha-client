4e33969f82b7b3f62271f4fbf5b9989d225957e8
Danny Sofftie <dankim761@gmail.com>
2018-12-17 10:19:45 +0300

    Add template definition form

---
+++ src/views/components/Categories/TemplateContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="my-5">
+    <div>{{ template.title }}</div>
+    <div class="py-3">
+      <div v-if="template.feedback == 'single-select'">
+        <div :name="template._id" v-for="(option, index) in template.options" :key="index">
+          <input type="radio" :id="index" :name="template._id" :value="index">
+          <label :for="index" class="mx-4">{{ option.option }}</label>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<!-- -->
+<script>
+export default {
+  props: {
+    template: null,
+    jobslug: null
+  },
+  mounted() {
+    console.log("Template mounted");
+  }
+};
+</script>
